02ad2614489dec239c46d6495d233648dff3480c
JanBencek <Bencek1122@gmail.com>
2025-12-15 15:15:26 +0100

    SonarQube analiza

--- src/main/java/bencit/com/restaurantmanager/controllers/EmployeeController.java
+++ src/main/java/bencit/com/restaurantmanager/controllers/EmployeeController.java
@@ -1,5 +1,53 @@
 package bencit.com.restaurantmanager.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 public class EmployeeController {
+
+    @FXML
+    private Button addEmployeeButton;
+
+    @FXML
+    private DatePicker dateOfBirthDatePicker;
+
+    @FXML
+    private TableColumn<?, ?> dateOfBirthOfEmployeeColumn;
+
+    @FXML
+    private TextField descriptionOfRoleOfEmployeeTextField;
+
+    @FXML
+    private TableColumn<?, ?> lastNameOfEmployeeColumn;
+
+    @FXML
+    private TextField lastNameOfEmployeeTextField;
+
+    @FXML
+    private TableColumn<?, ?> nameOfEmployeeColumn;
+
+    @FXML
+    private TextField nameOfEmployeeTextField;
+
+    @FXML
+    private TableColumn<?, ?> phoneNumberOfEmployeeColumn;
+
+    @FXML
+    private TextField phoneNumberOfEmployeeTextField;
+
+    @FXML
+    private TableColumn<?, ?> roleOfEmployeeColumn;
+
+    @FXML
+    private ComboBox<?> roleOfEmployeeComboBox;
+
+    @FXML
+    void addEmployee(ActionEvent event) {
+
+    }
 
 }
